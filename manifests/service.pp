9987dd25218f8e31aa61a77d7a5b860466f8b5ef
shubh <shubhendra.saxena@gmail.com>
2017-12-23 19:46:51 +0530

    dd
    ss

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,11 +1,13 @@
 class tomcat::service (
 	String $servicename = lookup({"name" => "service_name"})
+	String $servicen_state = lookup({"name" => "service_state"})
 	
 	) {
 
 	service {'service_name':
+	name =>   $service_name,
         ensure => $service_state,
-	    enable => true,
+	enable => true,
 
 }
 
