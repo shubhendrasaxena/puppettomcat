e93900142a32c3f98b193ae00d1d1b7cbeea726a
shubh <shubhendra.saxena@gmail.com>
2017-12-24 14:50:27 +0530

    ss

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,11 +1,13 @@
 class tomcat::config (
     $config_path = lookup(config_path),
+    $config_source = lookup(config_source),
 
 ) {
 
-        file{ 'config_path':
+        file{ 'config_tomcat':
         path    => $config_path,
-        content => template('tomcat/tomcat.conf.erb'),
+        ensure  => file,
+        source  => $config_source,
         mode    => $mode,
         owner   => $owner,
         group   => $group,
